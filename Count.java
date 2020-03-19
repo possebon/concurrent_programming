cd339d450e497d4ac86213d51ce1d0fc0b98299e
Fernando Possebon <possebon@gmail.com>
2020-03-19 12:12:09 -0300

    Add Count.java

---
+++ Count.java
@@ -0,0 +1,27 @@
+/* Copyright (C) 2006 M. Ben-Ari. See copyright.txt */
+
+class Count extends Thread {
+    static volatile int n = 0;
+
+    public void run() {
+      int temp;
+      for (int i = 0; i < 10000; i++) {
+        temp = n;
+        n = temp + 1;
+      }
+    }
+
+    public static void main(String[] args) {
+      Count p = new Count();
+      Count q = new Count();
+      p.start();
+      q.start();
+      try { p.join(); q.join(); }
+      catch (InterruptedException e) { }
+      System.out.println("The value of n is " + n);
+    }
+	
+    // o que se pode esperar do valor de n ao final da execução
+    // qual o problema?
+	
+}
