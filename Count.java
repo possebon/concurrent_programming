6fc6e19f7834b89560f4f0b088df0f68b1beee1c
Fernando Possebon <possebon@gmail.com>
2020-05-20 09:18:50 -0300

    changes on .gitignore

--- Count.java
+++ Count.java
@@ -5,20 +5,28 @@ class Count extends Thread {
 
     public void run() {
       int temp;
+      
+            
       for (int i = 0; i < 10000; i++) {
         temp = n;
         n = temp + 1;
       }
+
     }
 
     public static void main(String[] args) {
       Count p = new Count();
       Count q = new Count();
+      long before = System.nanoTime();
+
       p.start();
       q.start();
       try { p.join(); q.join(); }
       catch (InterruptedException e) { }
+      long after = System.nanoTime();
+      long time = after - before;
       System.out.println("The value of n is " + n);
+      System.out.println("The total time of execution is " + time + " nanoseconds");
     }
 	
     // o que se pode esperar do valor de n ao final da execução
